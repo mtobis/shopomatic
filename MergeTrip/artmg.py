99a19f9699cf8da9fbc5ad225dad65a1e6e663fe
mtobis <mtobis@gmail.com>
2022-05-12 12:36:27 -0400

    artmg argparse

--- MergeTrip/artmg.py
+++ MergeTrip/artmg.py
@@ -1,39 +1,23 @@
+import argparse
+
 verbose = True
 
 def Print(*args):
     if verbose:
         print(*args)
-
-def compheads(files):
-    
-    assert len(files) > 1
-            
-    results = []
+        
+def merge_csv(filenames,columnlist):
     allheaders = set()
-    
-    for nextfile in files:
-        assert len(nextfile) > 1
-        #headers = nextfile[0].split(",")
-        headers = nextfile[0]
-        allheaders.update(headers)
-
-        for item in nextfile[1:]:
-            #line = item.split(",")
-            #line = item
-            results.append(dict(zip(headers,item)))
-
-    # return allheaders
-    return allheaders,results
-
-def dropartists(filenames):
-    files = []
+    results = []
     for fnam in filenames:
         reader = csv.reader(open(fnam))
-        files.append(list(reader))
-    allheadset,results = compheads(files)
-    allheads = list(allheadset)
-    Print(allheads)
-
+        items = list(reader)
+        headers = items[0]
+        allheaders.update(headers)
+        for item in items[1:]:
+            results.append(dict(zip(headers,item)))
+    allheads = list(allheaders)
+    
     with open('artists.csv', 'w') as outfile:
         writer = csv.DictWriter(outfile,fieldnames=allheads)
         writer.writeheader()
@@ -42,27 +26,32 @@ def dropartists(filenames):
 
 if __name__ == "__main__":
     import csv
+    from os.path import exists
 
-    dropartists(["artist2.csv","artist1.csv"])
+    paramGet = argparse.ArgumentParser()
+    paramGet.add_argument("-d","--datalist",help="path to list of csvfiles")
+    paramGet.add_argument("-c","--columnlist",help="path to list of output columns")
 
-    """
-    filenames = ["artist2.csv","artist1.csv"]
-    files = []
-    for fnam in filenames:
-        reader = csv.reader(open(fnam))
-        files.append(list(reader))
-        #files.append(open(fnam).readlines())
-    allheadset,results = compheads(files)
-    allheads = list(allheadset)
-    Print(allheads)
-
-    with open('artists.csv', 'w') as outfile:
-        writer = csv.DictWriter(outfile,fieldnames=allheads)
-        writer.writeheader()
-        for result in results:
-            Print(result)
-            writer.writerow(result)
-    
-    #combined = [",".join(allheads)]
-    #Print(combined)
-    """
+    args = paramGet.parse_args()
+    if args.datalist:
+        if exists(args.datalist):
+            Print("found")
+            with open(args.datalist) as f:
+                flist = f.read().splitlines()
+        else:
+            raise FileNotFoundError
+    else:
+        flist = ["artist2.csv","artist1.csv"]
+        
+    if args.columnlist:
+        if exists(args.columnlist):
+            Print("found 2")
+            with open(args.columnlist) as f:
+                clist = f.read().splitlines()
+        else:
+            raise FileNotFoundError
+    else:
+        clist = None
+        
+    Print(flist)
+    merge_csv(flist,clist)
